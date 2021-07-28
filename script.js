14096100425768a5565ec00ca6ef22a074ca8a6c
masonhuh <mth517@gmail.com>
2021-07-28 00:34:04 -0500

    liking likes pt.2

---
+++ script.js
@@ -0,0 +1,23 @@
+var likeCounter = 9;
+var countElement = document.querySelector(".likeCounter");
+
+  function add1_Btn1(){
+      likeCounter++;
+      countElement.innerText = likeCounter + " like(s)";
+  }
+
+var likeCounter2 = 12;
+var countElement2 = document.querySelector(".likeCounter2");
+
+  function add1_Btn2(){
+      likeCounter2++;
+      countElement2.innerText = likeCounter2 + " like(s)";
+  }
+  
+var likeCounter3 = 9;
+var countElement3 = document.querySelector(".likeCounter3");
+
+  function add1_Btn3(){
+      likeCounter3++;
+      countElement3.innerText = likeCounter3 + " like(s)";
+  }
